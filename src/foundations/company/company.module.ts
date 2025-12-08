b4fcc7c290258469257ac734f4de1e89633cb216
Carlo Nicora <carlo.nicora@gmail.com>
2025-12-08 12:27:10 +0100

    fix: correct queue names

--- src/foundations/company/company.module.ts
+++ src/foundations/company/company.module.ts
@@ -3,20 +3,21 @@ import { Module, OnModuleInit } from "@nestjs/common";
 import { createWorkerProvider } from "../../common/decorators/conditional-service.decorator";
 import { modelRegistry } from "../../common/registries/registry";
 
+import { QueueId } from "../../config";
+import { FeatureModule } from "../feature/feature.module";
+import { S3Module } from "../s3/s3.module";
 import { CompanyController } from "./controllers/company.controller";
 import { CompanyModel } from "./entities/company.model";
 import { CompanyProcessor } from "./processors/company.processor";
 import { CompanyRepository } from "./repositories/company.repository";
 import { CompanySerialiser } from "./serialisers/company.serialiser";
 import { CompanyService } from "./services/company.service";
-import { FeatureModule } from "../feature/feature.module";
-import { S3Module } from "../s3/s3.module";
 
 @Module({
   controllers: [CompanyController],
   providers: [CompanyRepository, CompanyService, CompanySerialiser, createWorkerProvider(CompanyProcessor)],
   exports: [CompanyService, CompanySerialiser, CompanyRepository],
-  imports: [BullModule.registerQueue({ name: `${process.env.QUEUE}_company` }), FeatureModule, S3Module],
+  imports: [BullModule.registerQueue({ name: QueueId.COMPANY }), FeatureModule, S3Module],
 })
 export class CompanyModule implements OnModuleInit {
   onModuleInit() {
